f83cb96a23b260c693056519a2d6c567929432ea
Bryn Beaudry <babeaudry@gmail.com>
2018-01-12 16:49:55 -0800

    Updated dummy data to have Dad as the editor, created an admin and user role, assigned dad to the role admin, assigned a random user to the role member and created placeholders for dummy comments and posts

--- Data/DummyData.cs
+++ Data/DummyData.cs
@@ -4,199 +4,132 @@ using System.Linq;
 using System.Threading.Tasks;
 using api_thinkaboutitbc.Models;
 using api_thinkaboutitbc.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
 
 namespace api_thinkaboutitbc.Data
 {
     public class DummyData
     {
-        public static void Initialize(ApplicationDbContext db)
-        {/* 
-            if (!db.Users.Any())
+        public static void Initialize(ApplicationDbContext db, IServiceProvider services)
+        {
+            IServiceScopeFactory scopeFactory = services.GetRequiredService<IServiceScopeFactory>();
+
+            using (IServiceScope scope = scopeFactory.CreateScope())
             {
-                db.Users.Add(new ApplicationUser
-                {
-                    Email = "test@user.com",
-                    Bio = "Wanna play video games dawg",
-                    FirstName = "Test",
-                    LastName = "User",
-                    
-                });
+                RoleManager<Microsoft.AspNetCore.Identity.IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                UserManager<ApplicationUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                UserSeedAsync(db, roleManager, userManager);
             }
-            db.SaveChanges();
-
-            if (!db.Games.Any())
+            /*
+            if (!db.Activities.Any())
             {
-                db.Games.Add(new Game
-                {
-                    Title = "Super Smash Brothers",
-                    GameDescription = "The N64 Version of Super Smash Brothers",
-                    NumberOfPlayers = 4,
-                    ImageUrl = "http://www.yoshismoon.com/soundtracks/soundtrack-smash64.jpg",
-                    Url = "https://www.ssbwiki.com/Super_Smash_Bros."
-                });
-                db.Games.Add(new Game
-                {
-                    Title = "Marvel vs Capcom",
-                    GameDescription = "Marvel vs. Capcom is a series of crossover fighting games developed and published by Capcom, featuring characters from their own video game franchises and comic book series published by Marvel Comics.",
-                    NumberOfPlayers = 4,
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/6/62/MvCI_Cover.png",
-                    Url = "https://en.wikipedia.org/wiki/Marvel_vs._Capcom:_Infinite"
-                });
-                db.Games.Add(new Game
-                {
-                    Title = "Defense of the Ancients (DotA)",
-                    GameDescription = "Defense of the Ancients (DotA) is a multiplayer online battle arena mod for the video game Warcraft III: Reign of Chaos and its expansion, Warcraft III: The Frozen Throne. ",
-                    NumberOfPlayers = 16,
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/4/42/Dota75-loading-screen.png",
-                    Url = "https://en.wikipedia.org/wiki/Defense_of_the_Ancients"
-                });
-                db.Games.Add(new Game
-                {
-                    Title = "League of Legends",
-                    GameDescription = "League of Legends is a multiplayer online battle arena video game developed and published by Riot Games for Microsoft Windows and macOS.",
-                    NumberOfPlayers = 16,
-                    ImageUrl = "https://i.ytimg.com/vi/L7L7M_PPzJs/hqdefault.jpg",
-                    Url = "https://en.wikipedia.org/wiki/League_of_Legends"
-                });
-                db.Games.Add(new Game
-                {
-                    Title = "Super Street Fighter II Turbo",
-                    GameDescription = "Super Street Fighter II Turbo[a] is a competitive fighting game released for the arcades by Capcom in 1994. It is the fifth installment in the Street Fighter II sub-series of Street Fighter games, following Super Street Fighter II: The New Challengers.",
-                    NumberOfPlayers = 2,
-                    ImageUrl = "http://backinmyplay.com/wp-content/uploads/2015/12/cover_large.jpg",
-                    Url = "https://en.wikipedia.org/wiki/Super_Street_Fighter_II_Turbo"
-                });
-
+                db.Activities.AddRange(GetActivities().ToArray());
+                db.SaveChanges();
             }
-            db.SaveChanges();
-
-
             if (!db.Events.Any())
             {
-                //seed, call this function from ADBC
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing Smash @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Super Smash Brothers"),
-                    Title = "Crazy Super Smash Brothers Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing MVC @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Marvel vs Capcom"),
-                    Title = "Serious Marvel vs Capcom Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing Defense of the Ancients (DotA) @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Defense of the Ancients (DotA)"),
-                    Title = "Eighth Defense of the Ancients (DotA) Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing Super Street Fighter II Turbo @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Super Street Fighter II Turbo"),
-                    Title = "Second Super Street Fighter II Turbo Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing MVC @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Marvel vs Capcom"),
-                    Title = "First Marvel vs Capcom Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing League of Legends @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "League of Legends"),
-                    Title = "Super League of Legends Tournament at BCIT"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing Smash @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Super Smash Brothers"),
-                    Title = "Say what?! Super Smash Brothers Tournament at BCIT!?"
-                });
-                db.Events.Add(new Event
-                {
-                    Creator = db.Users.FirstOrDefault(u=> u.Email == "test@user.com"),
-                    Latitude = 49.248499,
-                    Longitude = -123.001375,
-                    LocationDescription = "BCIT",
-                    Host = "BCIT SA",
-                    StartTime = DateTime.Now.Add(new TimeSpan(24,0,0)),
-                    EndTime = DateTime.Now.Add(new TimeSpan(74, 0, 0)),
-                    EventDescription = "Playing Super Street Fighter II Turbo @ Bcit",
-                    Game = db.Games.FirstOrDefault(g=> g.Title == "Super Street Fighter II Turbo"),
-                    Title = "Super Street Fighter II Turbo Tournament at BCIT"
-                });
-                
-
-
+                db.Events.AddRange(GetEvents(db).ToArray());
+                db.SaveChanges();
             }
-            db.SaveChanges();
+            */
+        }
 
-            if (!db.EventGuests.Any())
+        public static async void UserSeedAsync(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            var isAdminRoleExist = await roleManager.RoleExistsAsync("Admin");
+            var isMemberRoleExist = await roleManager.RoleExistsAsync("Member");
+            if (!isAdminRoleExist)
             {
-                foreach (var item in db.Events.ToArray())
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (!isMemberRoleExist)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Member"));
+            }
+            if (await userManager.FindByNameAsync("a") == null)
+            {
+                var user = new ApplicationUser
                 {
-                    db.EventGuests.Add(new EventGuest
-                    {
-                        Guest = db.Users.FirstOrDefault(u => u.Email == "test@user.com"),
-                        Event = item
-                    });
+                    Email = "richardbeaudry@shaw.ca",
+                    UserName = "The_Editor",
+                    FirstName = "Richard",
+                    LastName = "Beaudry",
+                    ProviderName = "EMAIL"
+                };
+                var result = await userManager.CreateAsync(user, "mysonbrynisagenius");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
                 }
             }
-            db.SaveChanges(); */
+            if (await userManager.FindByNameAsync("a") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    Email = "a@a.a",
+                    UserName = "a"
+                };
+                var result = await userManager.CreateAsync(user, "");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Member");
+                }
+            }
         }
+
+        //Make Posts first
+
+        public static List<Post> GetPosts(ApplicationDbContext context)
+        {
+            List<Post> posts = new List<Post>()
+            {
+                new Post()
+                {
+
+                },
+            };
+            //context.SaveChanges();
+            return posts;
+        }
+
+        public static List<Comment> GetComments(ApplicationDbContext context)
+        {
+            List<Comment> comments = new List<Comment>()
+            {
+                new Comment()
+                {
+
+                },
+            };
+            //context.SaveChanges();
+            return comments;
+        }
+
+
+
+
+        /*
+        public static List<Event> GetEvents(ApplicationDbContext context)
+        {
+            List<Event> events = new List<Event>()
+            {
+                new Event()
+                {
+                    Activity = new Activity()
+                    {
+                        ActivityDescription = "Senior’s Golf Tournament"
+                    },
+                    StartDate = new DateTime(2017, 12, 22, 12, 00, 0),
+                    EndDate = new DateTime(2017, 12, 22, 13, 30, 0),
+                    IsActive = true,
+                    EnteredByUsername = "a",
+                },
+            //context.SaveChanges();
+            return events;
+        }
+        */
     }
 }
+
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # api-thinkaboutitbc
-# api-thinkaboutitbc
+# This is for ensuring that the correct version of OPenId Dict is used
+dotnet add package OpenIddict --version 2.0.0-rc2-0772 --source https://www.myget.org/F/aspnet-contrib/api/v3/index.json
+
+# Make sure that you export your deve environment
+
