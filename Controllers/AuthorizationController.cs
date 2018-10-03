a7f5da981dc3c652d873681a27599ebac09b192c
Bryn Beaudry <babeaudry@gmail.com>
2018-10-03 09:23:29 -0700

    providing better logs for local

--- Controllers/AuthorizationController.cs
+++ Controllers/AuthorizationController.cs
@@ -516,7 +516,7 @@ namespace api_thinkaboutitbc.Controllers
             if (fbDetails != null)
             {
                 // save 
-                Console.WriteLine($"Facebook details: {fbDetails.ToString()}, email: {fbDetails.Email}, id {fbDetails.ProviderUserId}");
+                Console.WriteLine($"Facebook details: {fbDetails.ToString()}, email: {fbDetails.Email}, id {fbDetails.ProviderUserId}, local : {fbDetails.Locale}");
                 //check to see if the ProviderId is already listed for this user
                 var repeatUser = _ctx.Users
                     .Where(x => x.ProviderId.Equals(fbDetails.ProviderUserId))
